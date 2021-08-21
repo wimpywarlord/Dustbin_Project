a4bc2fb41fc01feb987f024d776b4dce00d72bb0
wimpywarlord <wimpywarlord@gmail.com>
2021-08-22 00:29:18 +0530

    added file tempalte

--- app.js
+++ app.js
@@ -6,14 +6,33 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+'/public'));
 
+// mongoose.connect(
+
+//     "mongodb+srv://gec:noida132@cluster0.vrxpg.mongodb.net/<dbname>?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         dbName: "colleges",
+//     },
+//     async function (err, res) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Connected To DB");
+//             app.emit('ready'); 
+//         }
+//     }
+
+// );
+
+
 
 // DATA ENTRY
 app.get("/", function (req, res) {
-    res.send("BOOM CHICKA");
+    res.render("index.ejs");
 })
 
-app.on('ready', function() { 
-    app.listen(process.env.PORT || 3000, function () {
-        console.log("SERVER 3000 HAS STARTED");
-    });
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("SERVER 3000 HAS STARTED");
 });
